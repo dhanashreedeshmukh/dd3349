d7322b78506cf4911e8b9a4287d2db34687f4f74
dhanashreedeshmukh <dhanashreedeshmukh90@gmail.com>
2024-03-16 18:42:19 -0400

    Updates

---
+++ precache-manifest.d17a98936696d7e3187be9393bdf3376.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3bd57912532d4fcb4ce478443a2041f",
+    "url": "/dd3349/index.html"
+  },
+  {
+    "revision": "be6494cc1d154dbffaf9",
+    "url": "/dd3349/static/css/main.2a3812a6.chunk.css"
+  },
+  {
+    "revision": "eed30d54fd950c5622b5",
+    "url": "/dd3349/static/js/2.d6726511.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/dd3349/static/js/2.d6726511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5534b521a7627a0867a",
+    "url": "/dd3349/static/js/3.01278217.chunk.js"
+  },
+  {
+    "revision": "be6494cc1d154dbffaf9",
+    "url": "/dd3349/static/js/main.070a5f96.chunk.js"
+  },
+  {
+    "revision": "e230a4d6ce6c59355747",
+    "url": "/dd3349/static/js/runtime-main.0d176331.js"
+  }
+]);
